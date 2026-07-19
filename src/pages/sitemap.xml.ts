176a950668175e87da628a007e606f516ef78baa
hyeonseop <loveremain67@gmail.com>
2026-07-19 22:16:30 +0900

    code change

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,15 +1,32 @@
 import type { APIRoute } from 'astro';
+import { siteConfig } from '../config';
+import signs from '../data/signs.json';
+
+const staticPaths = [
+  '/',
+  '/about/',
+  '/guide/how-results-work/',
+  '/guide/zodiac-horoscope/',
+  '/horoscope/',
+  '/monthly/',
+  '/privacy/',
+  '/today/',
+  '/weekly/',
+  '/zodiac/'
+];
 
 export const GET: APIRoute = ({ site }) => {
-  const base = site ?? new URL('https://fortunedaily.co.kr');
+  const base = site ?? new URL(siteConfig.fallbackUrl);
+  const signPaths = signs.map((sign) =>
+    sign.type === 'zodiac' ? `/zodiac/${sign.slug}/` : `/horoscope/${sign.slug}/`
+  );
+  const urls = [...staticPaths, ...signPaths]
+    .map((path) => `<url><loc>${new URL(path, base).href}</loc></url>`)
+    .join('');
 
   return new Response(
     `<?xml version="1.0" encoding="UTF-8"?>
-<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <sitemap>
-    <loc>${new URL('sitemap-0.xml', base).href}</loc>
-  </sitemap>
-</sitemapindex>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${urls}</urlset>
 `,
     { headers: { 'Content-Type': 'application/xml; charset=utf-8' } }
   );
